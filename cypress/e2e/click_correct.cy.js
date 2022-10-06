818e0d5527163ed6a0b38b0ea17e920dda4fbbc3
Lukasz Kaczmarek <lukaszk76@gmail.com>
2022-10-06 09:04:23 +0200

    wrong cypress test added

--- cypress/e2e/click_correct.cy.js
+++ cypress/e2e/click_correct.cy.js
@@ -5,7 +5,7 @@ describe("Clicking is easy...", () => {
   });
 
   it("the dialog should disapear when Agree is clicked", () => {
-    cy.get(".dialog").should("be.visible");
+    cy.get(".dialog").should("be.visible").wait(500);
     cy.get("#agree").click();
     cy.get(".dialog").should("not.exist");
   });
